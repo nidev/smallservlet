c6100ac645a9e30d99f24b62bec74e464c47d92f
Nidev Plontra <nidev.plontra@gmail.com>
2017-02-02 23:10:03 +0900

    Add complete nocache driver and incomplete Redis driver

---
+++ lib/src/cache_driver/nocache.dart
@@ -0,0 +1,72 @@
+// encoding: utf-8
+import "package:smallservlet/src/cache_driver/base.dart";
+
+class NoCacheDriver implements BaseCacheDriver {
+  /**
+   * Return cache size in the number of Key-Value items
+   */
+  int getCacheSize() {
+    return 0;
+  }
+
+  /**
+   * Set cache size in the number of Key-Value items
+   */
+  void setCacheSize(int size) {
+    int _ = size;
+  }
+
+  /**
+   * Check whether cache already knows the key and confirms its valid lifetime.
+   */
+  bool hasValue(String key) {
+    return false;
+  }
+
+  /**
+   * Get value from cache. This may return number type value or String type value.
+   * If key is not in cache, returns null.
+   * If key is in cache and outdated, return null and key and its value will be removed.
+   */
+  dynamic operator[](String key) {
+    return null;
+  }
+
+  /**
+   * Compress cache immediately.
+   * NoCacheDriver does nothing.
+   */
+  void compress(bool brutally) {
+    bool _ = brutally;
+  }
+
+  /**
+   * Clear cache. NoCacheDriver does nothing.
+   */
+  void emptify() {
+    ;
+  }
+
+  /**
+   * Return whether this driver utilizes external softwares. Softwares can be
+   * databases like sqlite, Key-value storage like Redis.
+   */
+  bool hasBackbone() {
+    return false;
+  }
+
+  /**
+   * Check whether backbone is healthy and can interact.
+   */
+  bool checkBackbone() {
+    return false;
+  }
+
+  /**
+   * Recover backbone from malfunctioning like disconnection.
+   * NoCacheDriver does nothing.
+   */
+  bool recoverBackbone() {
+    return false;
+  }
+}
---
+++ lib/src/cache_driver/redis.dart
@@ -0,0 +1,73 @@
+// encoding: utf-8
+import "package:smallservlet/src/cache_driver/base.dart";
+import "package:redis/redis.dart";
+
+class RedisCacheDriver implements BaseCacheDriver {
+  /**
+   * Return cache size in the number of Key-Value items
+   */
+  int getCacheSize() {
+    throw new UnimplementedError();
+  }
+
+  /**
+   * Set cache size in the number of Key-Value items
+   */
+  void setCacheSize(int size) {
+    throw new UnimplementedError();
+  }
+
+  /**
+   * Check whether cache already knows the key and confirms its valid lifetime.
+   */
+  bool hasValue(String key) {
+    throw new UnimplementedError();
+  }
+
+  /**
+   * Get value from cache. This may return number type value or String type value.
+   * If key is not in cache, returns null.
+   * If key is in cache and outdated, return null and key and its value will be removed.
+   */
+  dynamic operator[](String key) {
+    throw new UnimplementedError();
+  }
+
+  /**
+   * Compress cache immediately.
+   * If set 'brutally', 50% of cached items will be removed even they are valid lifetime.
+   */
+  void compress(bool brutally) {
+    throw new UnimplementedError();
+  }
+
+  /**
+   * Clear cache. While running, Driver acquires internal cache lock and pauses threads.
+   */
+  void emptify() {
+    throw new UnimplementedError();
+  }
+
+  /**
+   * Return whether this driver utilizes external softwares. Softwares can be
+   * databases like sqlite, Key-value storage like Redis.
+   */
+  bool hasBackbone() {
+    throw new UnimplementedError();
+  }
+
+  /**
+   * Check whether backbone is healthy and can interact.
+   */
+  bool checkBackbone() {
+    throw new UnimplementedError();
+  }
+
+  /**
+   * Recover backbone from malfunctioning like disconnection.
+   * While running, Driver acquires internal cache lock and pauses threads.
+   */
+  bool recoverBackbone() {
+    throw new UnimplementedError();
+  }
+}
