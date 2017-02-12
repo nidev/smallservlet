b5fc296ec8f9800029753a34f8859702bc36d708
Nidev Plontra <nidev.plontra@gmail.com>
2017-02-12 17:16:03 +0900

    Add more guard tests and basic function test
    Also, not to use static Redis key. A time-based suffix will be appended to base Redis key

--- test/cache_driver/redis_test.dart
+++ test/cache_driver/redis_test.dart
@@ -2,10 +2,14 @@
 import "package:test/test.dart";
 import "package:smallservlet/src/cache_driver/redis.dart";
 
+const String DRIVER_TEST_KEY_PREFIX = "default_spec_test";
+
 void main(List<String> arguments) {
+  // Generate randomized suffix for
+  final String testKey = "${DRIVER_TEST_KEY_PREFIX}:${new DateTime.now().microsecond}";
 
   RedisCacheDriver redisCacheDriver;
-  redisCacheDriver = new RedisCacheDriver(redisKey: "default_spec_test");
+  redisCacheDriver = new RedisCacheDriver(redisKey: testKey);
   
   group("RedisCacheDriver default specifications test", () {
     test("Has default cache size",
@@ -26,6 +30,39 @@ void main(List<String> arguments) {
     });
     
     tearDown(() async {
+      await redisCacheDriver.recoverBackbone();
+    });
+  });
+
+  group("'Parameter guard' test", () {
+    test("Blank redis key throws exception at constructor", () {
+      expect(() => new RedisCacheDriver(redisKey: ""), throws);
+    });
+
+    test("Gets error on trying to set cache size to number less than zero", () {
+      // TODO: Test with more negative numbers
+      expect(() => redisCacheDriver.setCacheSize(-1), throws);
+    });
+
+    test("Gets error on trying to set lifetime seconds to number less than zero", () {
+      // TODO: Test with more negative numbers
+      expect(() => redisCacheDriver.setLifetimeSeconds(-1), throws);
+    });
+  });
+
+  group("'Basic cache function' test", () {
+    setUp(() async {
+      await redisCacheDriver.checkBackbone();
+    });
+
+    test("Tries to save an item", () async {
+      // XXX: operator[] is not an async/await function. There is a possibility of cache miss
+      await redisCacheDriver.store("fireball", "1");
+      expect(redisCacheDriver.hasValue("fireball"), completion(equals(true)));
+    });
+
+    tearDown(() async {
+      await redisCacheDriver.emptify();
       await redisCacheDriver.recoverBackbone();
     });
   });
