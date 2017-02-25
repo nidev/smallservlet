1ec7e7c9c66ecca512b4b46a9a6ef77c808b8e18
Nidev Plontra <nidev.plontra@gmail.com>
2017-02-25 22:35:14 +0900

    Update doc comments in rule.dart
    Specifies concrete behaviors of each ROUTE_COMMANDs.

--- lib/src/route_component/rule.dart
+++ lib/src/route_component/rule.dart
@@ -1,20 +1,47 @@
 // encoding: utf-8
 import "dart:io";
+import "package:smallservlet/src/route_component/pattern_compiler.dart";
 
-enum ROUTE_COMMAND { STATIC, DART_SERVLET, DART_SERVLET_NO_PARAM, DENIED, REDIRECT, FORWARD }
-enum METHODS { GET, POST, PUT, DELETE }
+/// Available actions on SmallServlet routing rule
+enum ROUTE_COMMAND {
+  /// Serves static datum/file in exact rootdir path (i.e. http://localhost/imgs/image.png will serve $rootdir/imgs/image.png)
+  STATIC,
+  /// Serves Dart-based servlet in Isolated context (See [Dart Isolate API](https://api.dartlang.org/stable/1.22.1/dart-isolate/Isolate-class.html) on official site)
+  DART_SERVLET,
+  /// Serves Dart-based servlet in Isolated context, but wipes all params (represented as HTTP query string) before executing.
+  DART_SERVLET_NO_PARAM,
+  /// Will serve error page with error code instead of executing or serving a content
+  DENIED,
+  /// Redirects to another Dart-based servlet or static content
+  REDIRECT,
+  /// Requested servlet will not handle request, instead, preserving all parameters, delegated servlet or static content will be done/provided.
+  FORWARD
+}
 
-/**
- * URL Route rule class
- */
+/// Predefined allowed HTTP methods
+enum METHODS {
+  /// HTTP GET method
+  GET,
+  /// HTTP POST method
+  POST,
+  /// HTTP PUT method
+  PUT,
+  /// HTTP DELETE method
+  DELETE
+}
+
+/// URL Routing rule for SmallServlet and router.dart
 class Rule {
   final ROUTE_COMMAND command;
   final Set<METHODS> acceptedMethods;
-  final String pattern;
+  final URLPattern pattern;
   final String nextRoute;
 
-  Rule(this.command, this.acceptedMethods, this.pattern, this.nextRoute);
-
+  Rule(ROUTE_COMMAND route_command, Set<METHODS> methods, String stringPattern, String nextRoutePath) :
+    command = route_command,
+    acceptedMethods = methods,
+    pattern = new URLPattern.compileFromString(stringPattern),
+    nextRoute = nextRoutePath;
 
   String onRedirect(HttpRequest req, HttpResponse res) {
     throw new UnimplementedError();
