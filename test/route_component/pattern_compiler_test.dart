0fad6a94c844fef5da343a8ab831c7c6ff01b6a2
Nidev Plontra <nidev.plontra@gmail.com>
2017-03-02 23:12:19 +0900

    Add 'No default constructor test' for pattern_compiler.dart

--- test/route_component/pattern_compiler_test.dart
+++ test/route_component/pattern_compiler_test.dart
@@ -3,16 +3,34 @@ import "package:test/test.dart";
 import "package:smallservlet/src/route_component/pattern_compiler.dart";
 
 void main(List<String> args) {
+  URLPattern pattern;
+
   group("'No default constructor' test", () {
     test("Throws an Exception when default constructor is called",
       () => expect(() => new URLPattern(), throws));
+
+    test("Throws nothing when proper constructor is called", () {
+      pattern = new URLPattern.compileFrom("/", "/");
+    });
   });
 
-  group("'URL path sanity check' test", () {
+  group("'URL without template path compilation' test", () {
+    test("Compile / => /index.dart", () {
+       pattern = new URLPattern.compileFrom("/", "/");
+
+       expect(pattern.compiledPath, equals("/index.dart"));
+    });
+  });
+
+  group("'URL with template path compilation' test", () {
 
   });
 
   group("'Allowed URL pattern' test", () {
+
+  });
+
+  group("'Disallowed URL pattern' test", () {
 
   });
 }
