f7d208b4511f88c0b2b188e784b57bfd91e4b7b4
Nidev Plontra <nidev.plontra@gmail.com>
2017-03-16 22:34:23 +0900

    Fix all test case to follow new accessing method

--- test/route_component/compiler_test.dart
+++ test/route_component/compiler_test.dart
@@ -15,10 +15,10 @@ void main(List<String> args) {
     test("Checks query string is converted to Map<String, String>", () {
       var params = compiler.parse("/?fruit=apple&count=1");
 
-      expect(param, isNotNull);
-      expect(param, isNotEmpty);
-      expect(param["fruit"], equals("apple"));
-      expect(param["count"], equals("1"));
+      expect(params, isNotNull);
+      expect(params, isNotEmpty);
+      expect(params["fruit"], equals("apple"));
+      expect(params["count"], equals("1"));
     });
   });
 
@@ -40,12 +40,12 @@ void main(List<String> args) {
 
     test("Compiles /* with /, it is matched", () {
        compiler = new URLPatternCompiler("/*");
-       expect(compiler.parse("/"), isNotNull); // does not throw
+       expect(compiler.parse("/"), isNotNull);
     });
 
     test("Compiles /* with /users/, it is matched", () {
       compiler = new URLPatternCompiler("/*");
-      expect(compiler.parse("/users/"), isNotNull); // does not throw
+      expect(compiler.parse("/users/"), isNotNull);
     });
 
     test("Compiles /users/ => /users/index.dart", () {
@@ -55,32 +55,38 @@ void main(List<String> args) {
 
     test("Compiles /users/* with /users/foo, it is matched", () {
       compiler = new URLPatternCompiler("/users/*");
-      expect(compiler.parse("/users/foo"), isNotEmpty);
+      expect(compiler.parse("/users/foo"), isNotNull);
     });
 
     test("Compiles /users/index.dart => /users/index.dart", () {
-       compiler = new URLPatternCompiler("/users/index.dart", "/users/index.dart");
+       compiler = new URLPatternCompiler("/users/index.dart");
        expect(compiler.dartLocation, equals("/users/index.dart"));
     });
   });
 
   group("'URL with template path compilation' test", () {
     test("Compiles /users/{name} with /users/foo => /users.dart with params {name: foo}", () {
-       compiler = new URLPatternCompiler("/users/{name}", "/users/foo");
+       compiler = new URLPatternCompiler("/users/{name}");
+
+       var params = compiler.parse("/users/foo");
+
        expect(compiler.dartLocation, equals("/users.dart"));
-       expect(pattern.compiledParam, isNotEmpty);
-       expect(pattern.compiledParam.containsKey("name"), isTrue);
-       expect(pattern.compiledParam["name"], equals("foo"));
+       expect(params, isNotEmpty);
+       expect(params.containsKey("name"), isTrue);
+       expect(params["name"], equals("foo"));
     });
 
     test("Compiles /users/{name}/{age} with /users/foo/15 => /users.dart with params {name: foo, age: 15}", () {
-       compiler = new URLPatternCompiler("/users/{name}/{age}", "/users/foo/15");
+       compiler = new URLPatternCompiler("/users/{name}/{age}");
+
+       var params = compiler.parse( "/users/foo/15");
+
        expect(compiler.dartLocation, equals("/users.dart"));
-       expect(pattern.compiledParam, isNotEmpty);
-       expect(pattern.compiledParam.containsKey("name"), isTrue);
-       expect(pattern.compiledParam["name"], equals("foo"));
-       expect(pattern.compiledParam.containsKey("age"), isTrue);
-       expect(pattern.compiledParam["age"], equals("15"));
+       expect(params, isNotEmpty);
+       expect(params.containsKey("name"), isTrue);
+       expect(params["name"], equals("foo"));
+       expect(params.containsKey("age"), isTrue);
+       expect(params["age"], equals("15"));
     });
   });
 
@@ -89,32 +95,34 @@ void main(List<String> args) {
   });
 
   group("'Disallowed URL pattern' test", () {
-    test("Does not allow using .. or . in pattern", () {
-      expect(() => new URLPatternCompiler("/../*", "/"), throws);
-      expect(() => new URLPatternCompiler("/./*", "/"), throws);
-      expect(() => new URLPatternCompiler("/../../", "/"), throws);
+    test("Does not allow using canonical path in pattern", () {
+      expect(() => new URLPatternCompiler("/../*"), throws);
+      expect(() => new URLPatternCompiler("/./*"), throws);
+      expect(() => new URLPatternCompiler("/../../"), throws);
     });
 
-    test("Does not allow using .. or . in path", () {
-      expect(() => new URLPatternCompiler("/{dummy}", "/.."), throws);
-      expect(() => new URLPatternCompiler("/{dummy}", "/."), throws);
-      expect(() => new URLPatternCompiler("/{dummy}/", "/../index.dart"), throws);
+    test("Does not allow using canonical path in path part", () {
+      compiler = new URLPatternCompiler("/*");
+
+      expect(() => compiler.parse("/.."), throws);
+      expect(() => compiler.parse("/."), throws);
+      expect(() => compiler.parse("/../index.dart"), throws);
     });
 
     test("Throws on re-occuring of path literal in pattern after template begins", () {
-      expect(() => new URLPatternCompiler("/a/{b}/c", "/a/b/c"), throws);
+      expect(() => new URLPatternCompiler("/a/{b}/c"), throws);
     });
 
     test("Throws on space or control character in template name", () {
-      expect(() => new URLPatternCompiler("/users/{first name}", "/users/charles"), throws);
-      expect(() => new URLPatternCompiler("/users/{space\tlocation}", "/users/earth"), throws);
-      expect(() => new URLPatternCompiler("/users/{\r\n}", "/users/whatdidyousay"), throws);
+      expect(() => new URLPatternCompiler("/users/{first name}"), throws);
+      expect(() => new URLPatternCompiler("/users/{space\tlocation}"), throws);
+      expect(() => new URLPatternCompiler("/users/{\r\n}"), throws);
     });
 
     test("Throws on non-alphanumeric template names in pattern", () {
-      expect(() => new URLPatternCompiler("/users/{이름}", "/users/charles"), throws);
-      expect(() => new URLPatternCompiler("/users/{場所}", "/users/earth"), throws);
-      expect(() => new URLPatternCompiler("/users/{AAアア11}", "/users/whatdidyousay"), throws);
+      expect(() => new URLPatternCompiler("/users/{이름}"), throws);
+      expect(() => new URLPatternCompiler("/users/{場所}"), throws);
+      expect(() => new URLPatternCompiler("/users/{AAアア11}"), throws);
     });
   });
 }
